eca58078b19db1178b5d13029dd3d397155ee8e4
Developer <dev@example.com>
2026-03-30 11:03:43 +0800

    Revert "fix: 关闭 standalone 模式以适配宝塔面板的默认启动行为"
    This reverts commit f5cc86b53fefb2923bf4e03f320cce9316644c09.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'standalone', // 关闭 standalone 模式，避免宝塔面板找不到 .next 构建产物
+  output: 'standalone', // 开启独立部署模式，极大缩小 Docker 镜像体积，适合字节内部部署
   experimental: {
     serverComponentsExternalPackages: ['@prisma/client'],
   },
